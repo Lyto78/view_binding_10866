1c02996666f29a837f9a742def4ec0794a5dc66f
Lyto78 <fransiscodiky@gmail.com>
2022-09-13 08:50:04 +0700

    Kumpul GD Minggu Ke - 4

---
+++ app/src/main/java/com/example/view_binding_10866/TaskList.kt
@@ -0,0 +1,10 @@
+package com.example.view_binding_10866
+
+object TaskList {
+    val TaskList = listOf<Task>(
+        Task("Chapter 1","Bangun Tidur Dengan Semangat","Dibangunkan Oleh Alarm Perang"),
+        Task("Chapter 2","Share Di Internet","Share Propaganda Untuk Perang"),
+        Task("Chapter 3","Buat Senjata Nuklir","Buat senjata Nuklir untuk perang"),
+        Task("Chapter 4","Nonton Perang","Nonton perang sambil bersantai")
+    )
+}
